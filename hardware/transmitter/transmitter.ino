98629ffe08a3548737a4dac0fa8786ceae03b747
JoshQuaintance <hasianjoshua@gmail.com>
2025-09-04 09:42:56 -0400

    Update initial data

--- hardware/transmitter/transmitter.ino
+++ hardware/transmitter/transmitter.ino
@@ -7,27 +7,37 @@ void setup() {
 
   irsend.begin(IR_PIN);
 
+  Serial.begin(115200);
+  Serial.setTimeout(.5);
+
 }
+
+int x;
 
 void loop() {
 
-  // const char* message = "";
-  const char* message[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x0A, 0x2F, 0xBA, 0xFF};
-  // irsend.sendNEC(0x62, 16);
+  while (!Serial.available());
 
-  for (int i = 0; i < 10; i++) {
-  // for (int i = 0; message[i] != '\0'; i++) {
-    // char c = message[i];
-    
-    irsend.sendNEC(0x0168, message[i], 0);
-    
-    delay(50);
-    
-  }
-
-  irsend.sendNEC(0x00, HEX);
-
-  delay(1000);
+  int value = Serial.read();
 
 
+  irsend.sendNEC(0x0168, value, 0);
+  Serial.println(value, HEX);
+
+  delay(50);
+
+  
 }
+
+// const char* message[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x0A, 0x2F, 0xBA, 0xFF};
+
+// for (int i = 0; i < 10; i++) {
+//   irsend.sendNEC(0x0168, message[i], 0);
+  
+//   delay(50);
+  
+// }
+
+// irsend.sendNEC(0x00, HEX);
+
+// delay(1000);
